f30c7923c85fdb71c1d882564a3949c66a471fcf
santiagogarciia <santiagotgarcia464@gmail.com>
2023-06-17 17:56:43 -0300

    Initial commit

---
+++ Boton/boton.js
@@ -0,0 +1,7 @@
+const miBoton = document.getElementById("miBoton");
+
+function handleClick() {
+    alert("¡Hola! Has hecho click en el botón."); 
+}
+
+miBoton.addEventListener("click", handleClick)
